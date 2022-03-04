125b71ad37c0893d50a86a51e7fd6f60d220d902
77samp77 <77samp77@gmail.com>
2022-03-05 01:24:12 +0900

    csvでの英単語生成、当たり判定の変更

--- Assets/Scripts/WordController.cs
+++ Assets/Scripts/WordController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WordController : MonoBehaviour
 {
-    public GameObject /*canvas, */text, mark;
+    public GameObject textObject, mark;
     public float v;
     Vector3 pos;
     public bool isMarked;
@@ -12,8 +13,13 @@ public class WordController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // canvas = transform.Find("Canvas").gameObject;
-        // canvas.GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    public void BeSetWord(string wordStr){
+        textObject.GetComponent<Text>().text = wordStr;
+        int colWidth = 5 * wordStr.Length;
+        GetComponent<BoxCollider2D>().size = new Vector2(colWidth, 8);
+        mark.GetComponent<RectTransform>().sizeDelta = new Vector2(colWidth, 7);
     }
 
     // Update is called once per frame
@@ -36,7 +42,7 @@ public class WordController : MonoBehaviour
                                               Mathf.RoundToInt(cashPos.y),
                                               Mathf.RoundToInt(cashPos.z));
         pos = transform.localPosition;
-        text.transform.localPosition = new Vector3(pos.x + 79, pos.y - 12, pos.z);
+        textObject.transform.localPosition = new Vector3(pos.x + 79, pos.y - 12, pos.z);
         if(isMarked) mark.transform.localPosition = new Vector3(pos.x - 1, pos.y + 1, pos.z);
     }
 
--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class WordGenerator : MonoBehaviour
 {
+    public TextAsset wordsCSV;
+    List<string[]> wordsData = new List<string[]>();
+    int[] wordLen, wordNo;
+    int wordLen_max, wordNo_max;
+    string[,] wordStr;
+
+
     public GameObject wordPrefab, words;
     public int gen_interval;  // 生成間隔(フレーム数)
     int frame_gen;  // 生成時のフレーム
@@ -11,7 +19,26 @@ public class WordGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ReadWordsCSV();
+    }
+
+    void ReadWordsCSV(){
+        StringReader reader = new StringReader(wordsCSV.text);
+        while (reader.Peek() != -1){
+            string line = reader.ReadLine();
+            wordsData.Add(line.Split(','));
+        }
+
+        wordLen = new int[wordsData.Count];
+        wordNo = new int[wordsData.Count];
+        for(int i = 0; i < wordsData.Count; i++){
+            wordLen[i] = int.Parse(wordsData[i][1]);
+            wordNo[i] = int.Parse(wordsData[i][2]);
+            if(wordLen[i] > wordLen_max) wordLen_max = wordLen[i];
+            if(wordNo[i] > wordNo_max) wordNo_max = wordNo[i];
+        }
+        wordStr = new string[wordLen_max + 1, wordNo_max + 1];
+        for(int i = 0; i < wordsData.Count; i++) wordStr[wordLen[i], wordNo[i]] = wordsData[i][3];
     }
 
     // Update is called once per frame
@@ -28,5 +55,11 @@ public class WordGenerator : MonoBehaviour
         word.name = wordPrefab.name;
         word.transform.SetParent(words.transform);
         frame_gen = Time.frameCount;
+        SetWord(word, Random.Range(3, wordLen_max + 1), Random.Range(0, wordNo_max + 1));
+    }
+
+    void SetWord(GameObject word, int len, int no){
+        WordController wcs = word.GetComponent<WordController>();
+        wcs.BeSetWord(wordStr[len, no]);
     }
 }
