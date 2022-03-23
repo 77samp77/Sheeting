0d8f4853d9c9a2fe85e05ec8622d1a924028a5ad
77samp77 <77samp77@gmail.com>
2022-03-24 00:36:40 +0900

    NCMB準備、シーンTitleのボタン

--- Assets/Scripts/Manager/TitleManager.cs
+++ Assets/Scripts/Manager/TitleManager.cs
@@ -7,9 +7,16 @@ using UnityEngine.UI;
 public class TitleManager : MonoBehaviour
 {
     // GameObject screenManager;
-    public GameObject pressSpaceKey, backGround, systemSound;
+    public GameObject accountManager;
+    AccountManager ams;
+    public GameObject pressSpaceKey, titleButtons;
+    public GameObject backGround, systemSound;
     SystemSoundManager ssms;
     BackGroundController bgcs;
+
+    public GameObject[] choosingUI;
+    bool buttonIsAppear;
+    int choosing;
 
     void Awake(){
         Application.targetFrameRate = 60;
@@ -23,6 +30,7 @@ public class TitleManager : MonoBehaviour
     }
 
     void InitVariables(){
+        ams = accountManager.GetComponent<AccountManager>();
         bgcs = backGround.GetComponent<BackGroundController>();
         systemSound = GameObject.Find("SystemSound");
         ssms = systemSound.GetComponent<SystemSoundManager>();
@@ -31,12 +39,49 @@ public class TitleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!buttonIsAppear){
+            if(Time.frameCount % 100 == 0) pressSpaceKey.SetActive(true);
+            else if(Time.frameCount % 100 == 50) pressSpaceKey.SetActive(false);
+            if(Input.GetKeyDown(KeyCode.Space)) OpenButtons();
+        }
+        else ControllButtons();
+    }
+
+    void OpenButtons(){
+        ssms.PlaySE(ssms.SE_decide);
+        buttonIsAppear = true;
+        titleButtons.SetActive(true);
+    }
+
+    int prev_bottom = 1;
+    void ControllButtons(){
         if(Input.GetKeyDown(KeyCode.Space)){
-            ssms.PlaySE(ssms.SE_decide);
-            SceneManager.LoadScene("LevelSelect");
+            if(choosing == 0){
+                ssms.PlaySE(ssms.SE_decide);
+                SceneManager.LoadScene("LevelSelect");
+            }
         }
 
-        if(Time.frameCount % 100 == 0) pressSpaceKey.SetActive(true);
-        else if(Time.frameCount % 100 == 50) pressSpaceKey.SetActive(false);
+        int prev_choosing = choosing;
+        if(Input.GetKeyDown(KeyCode.W)){
+            if(choosing != 0){
+                prev_bottom = choosing;
+                choosing = 0;
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.S)){
+            if(choosing == 0) choosing = prev_bottom;
+        }
+        else if(Input.GetKeyDown(KeyCode.A)){
+            if(choosing == 2) choosing = 1;
+        }
+        else if(Input.GetKeyDown(KeyCode.D)){
+            if(choosing == 1) choosing = 2;
+        }
+        if(choosing != prev_choosing){
+            ssms.PlaySE(ssms.SE_choose);
+            choosingUI[prev_choosing].SetActive(false);
+            choosingUI[choosing].SetActive(true);
+        }
     }
 }
